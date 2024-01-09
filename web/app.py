9611f417c6dc8be4494a6d9520db6bd3fd9dcbd1
MephisDemoo <1451590580@qq.com>
2024-01-09 21:33:59 +0800

    Most Function Realized

--- web/app.py
+++ web/app.py
@@ -1,68 +1,59 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, send_file
+from flask import Flask, render_template, request, redirect, url_for,send_from_directory
 import os
 from werkzeug.utils import secure_filename
 import torch
 from torchvision import transforms
 from PIL import Image
 
-app = Flask(__name__)
 
 model_path = os.path.join('..', 'model', 'demo_resnet18')
-model = torch.load(r'../model/demo_resnet18')
+model = torch.load(model_path)
 
-upload_path = r'../uploads'
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-if not os.path.exists(upload_path):
+if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 def allowed_file(filename):
-    ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    ALLOWED_EXTENSIONS = {'jpg','jpeg','png','gif'}
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 def MyPredict(image_path):
-    label_list = [
-        'Biological',
-        'Fibres',
-        'Films_Coated_Surface',
-        'MEMS_devices_and_electrodes',
-        'Nanowires',
-        'Particles',
-        'Patterned_surface',
-        'Porous_Sponge',
-        'Powder',
-        'Tips'
-    ]
-
+    label_list = ['Biological','Fibres','Films_Coated_Surface','MEMS_devices_and_electrodes','Nanowires','Particles','Patterned_surface','Porous_Sponge','Powder','Tips']
+    # Your data transformation
     normal_transforms = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize(
-            [0.485, 0.456, 0.406],
-            [0.229, 0.224, 0.225]
-        )
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
     model.eval()
-
+    # Load and transform the single image
+    
     image = Image.open(image_path)
     input_image = normal_transforms(image)
-    input_image = input_image.unsqueeze(0)
+    input_image = input_image.unsqueeze(0)  # Add a batch dimension
 
+    # Make prediction
     with torch.no_grad():
         output = model(input_image.cuda())
-    
+
+    # Get the predicted label
     _, predicted_label = torch.max(output, 1)
 
-    print(f'Predicted label:  {label_list[predicted_label.item()]}')
-
+    print(f"Predicted label:  {label_list[predicted_label.item()]}")
+    
     return label_list[predicted_label.item()]
+ 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=["POST"])
 def upload_file():
     if 'file' not in request.files:
         return redirect(request.url)
@@ -72,19 +63,15 @@ def upload_file():
     
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file_path = os.path.join('../uploads', filename)
-        print('upload file' + file_path)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
         result = MyPredict(file_path)
         return render_template('index.html', filename=filename, result=result)
 
-@app.route('/uploads/<filename>')
+@app.route('/uploads/<filename>') # uploads/L6_00f153d186dbf5fa02e0558e47537fde.jpg
 def uploaded_file(filename):
-    filename = secure_filename(filename)
-    file_path = os.path.join('../uploads', filename)
-    print('uploaded file' + file_path)
-    return send_file(file_path)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
